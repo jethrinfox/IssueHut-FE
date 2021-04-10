03f1812e9cc12073a9d88f9a1dcbe8d571b5c796
jethrinfox <jethrinfox@gmail.com>
2021-04-10 15:06:27 -0300

    feat: 🎸 prettier && eslint setup

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,9 @@
-import Link from 'next/link'
-import Layout from '../components/Layout'
+import { NextPage } from "next";
 
-const IndexPage = () => (
-  <Layout title="Home | Next.js + TypeScript Example">
+const IndexPage: NextPage = () => (
+  <div>
     <h1>Hello Next.js 👋</h1>
-    <p>
-      <Link href="/about">
-        <a>About</a>
-      </Link>
-    </p>
-  </Layout>
-)
+  </div>
+);
 
-export default IndexPage
+export default IndexPage;
