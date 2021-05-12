32480044d1cff3ae8daee15a5757a675167fc05d
jethrinfox <jethrinfox@gmail.com>
2021-05-12 12:17:31 -0300

    feat: 🎸 changed isAuth to permit passing a url to redirect

--- src/hooks/useIsAuth.ts
+++ src/hooks/useIsAuth.ts
@@ -2,12 +2,16 @@ import { useRouter } from "next/router"
 import { useEffect } from "react"
 import { useMeQuery } from "../generated/graphql"
 
-export const useIsAuth = () => {
+export const useIsAuth = (customRoute?: string): void => {
   const { data, loading } = useMeQuery()
   const router = useRouter()
   useEffect(() => {
-    if (!loading && !data?.me) {
-      router.replace("/login?next=" + router.pathname)
+    if (!loading) {
+      if (!data?.me) {
+        router.replace("/login?next=" + router.pathname)
+      } else if (customRoute) {
+        router.replace(customRoute)
+      }
     }
-  }, [loading, data, router])
+  }, [loading, data, router, customRoute])
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,24 +1,11 @@
-import { Box, Heading } from "@chakra-ui/layout"
+import Layout from "components/Layout"
+import { useIsAuth } from "hooks/useIsAuth"
 import { NextPage } from "next"
-import ProjectsContainer from "../components/Projects/ProjectsContainer"
-import Layout from "../components/Layout"
-import { useIsAuth } from "../hooks/useIsAuth"
 
 const IndexPage: NextPage = () => {
-  useIsAuth()
+  useIsAuth("/projects/")
 
-  return (
-    <Layout>
-      <Box marginTop="3rem" display="flex" flexDir="column">
-        <Heading textAlign="center" marginBottom="2rem">
-          Projects
-        </Heading>
-        <Box>
-          <ProjectsContainer />
-        </Box>
-      </Box>
-    </Layout>
-  )
+  return <Layout></Layout>
 }
 
 export default IndexPage
